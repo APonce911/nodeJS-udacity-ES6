baae7f63b1f0c662dacd581fed2c129cb8e3fcd5
Airton Ponce <airtonponce@outlook.com>
2018-12-17 15:01:51 -0200

    define key and value variables

--- 03-built-ins/04-maps.js
+++ 03-built-ins/04-maps.js
@@ -9,6 +9,10 @@ members.set('Liam', 20.16);
 members.set('Sophia', 0);
 members.set('Marcus', 10.25);
 
+    let key = undefined
+    let value = undefined
 for (const member of members) {
-    // console.log(key, value);
+  key = member[0]
+  value = member[1]
+  console.log(key, value);
 }
