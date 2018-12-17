a2a709dcd7a71f62418c920a082a75f1a1ac6184
Airton Ponce <airtonponce@outlook.com>
2018-12-17 11:21:41 -0200

    add strings to set

--- 03-built-ins/sets.js
+++ 03-built-ins/sets.js
@@ -11,4 +11,9 @@
  */
 const myFavoriteFlavors = new Set();
 
+myFavoriteFlavors.add('chocolate chip');
+myFavoriteFlavors.add('cookies and cream');
+myFavoriteFlavors.add('strawberry');
+myFavoriteFlavors.add('vanilla');
 
+// myFavoriteFlavors.values();
