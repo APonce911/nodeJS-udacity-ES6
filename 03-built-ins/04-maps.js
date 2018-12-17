638d5a4b2d2fb7ed9ef1ee9904d9859c0c74c5d6
Airton Ponce <airtonponce@outlook.com>
2018-12-17 15:06:58 -0200

    use array descructuring

--- 03-built-ins/04-maps.js
+++ 03-built-ins/04-maps.js
@@ -9,10 +9,11 @@ members.set('Liam', 20.16);
 members.set('Sophia', 0);
 members.set('Marcus', 10.25);
 
-    let key = undefined
-    let value = undefined
+let [key,value] = []
+// let key = undefined
+// let value = undefined
+
 for (const member of members) {
-  key = member[0]
-  value = member[1]
+  [key,value] = member
   console.log(key, value);
 }
