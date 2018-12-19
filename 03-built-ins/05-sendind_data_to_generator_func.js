c2e988eb615be7c104838247b237b765592333ec
Airton Ponce <airtonponce@outlook.com>
2018-12-19 18:51:34 -0200

    working with xy instead of name on .next variable

--- 03-built-ins/05-sendind_data_to_generator_func.js
+++ 03-built-ins/05-sendind_data_to_generator_func.js
@@ -2,9 +2,9 @@ function* getEmployee() {
     const names = ['Amanda', 'Diego', 'Farrin', 'James', 'Kagure', 'Kavita', 'Orit', 'Richard'];
     const facts = [];
 
-    for (const z of names) {
+    for (const name of names) {
         // yield *out* each name AND store the returned data into the facts array
-        facts.push(yield z);
+        facts.push(yield name);
     }
 
     return facts;
@@ -13,27 +13,27 @@ function* getEmployee() {
 const generatorIterator = getEmployee();
 
 // get the first name out of the generator
-let name = generatorIterator.next().value;
+let xy = generatorIterator.next().value;
 
-// pass data in *and* get the next name
-name = generatorIterator.next(`${name} is cool!`).value;
+// pass data in *and* get the next xy
+xy = generatorIterator.next(`${xy} is cool!`).value;
 
-// pass data in *and* get the next name
-name = generatorIterator.next(`${name} is awesome!`).value;
+// pass data in *and* get the next xy
+xy = generatorIterator.next(`${xy} is awesome!`).value;
 
-// pass data in *and* get the next name
-name = generatorIterator.next(`${name} is stupendous!`).value;
+// pass data in *and* get the next xy
+xy = generatorIterator.next(`${xy} is stupendous!`).value;
 
 // you get the idea
-name = generatorIterator.next(`${name} is rad!`).value;
-name = generatorIterator.next(`${name} is impressive!`).value;
-name = generatorIterator.next(`${name} is stunning!`).value;
-name = generatorIterator.next(`${name} is awe-inspiring!`).value;
+xy = generatorIterator.next(`${xy} is rad!`).value;
+xy = generatorIterator.next(`${xy} is impressive!`).value;
+xy = generatorIterator.next(`${xy} is stunning!`).value;
+xy = generatorIterator.next(`${xy} is awe-inspiring!`).value;
 
 // pass the last data in, generator ends and returns the array
-const positions = generatorIterator.next(`${name} is magnificent!`).value;
+const positions = generatorIterator.next(`${xy} is magnificent!`).value;
 
-// displays each name with description on its own line
+// displays each xy with description on its own line
 positions.join('\n');
 
 console.log(positions)
